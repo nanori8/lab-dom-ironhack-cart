2352d2368dab179d7e5f49d34855b9cc66044903
nanori <leonor.ferreira@gmail.com>
2020-04-21 20:24:31 +0100

    All mandatory iterations done. Trying to solve Bonus 4

--- js/index.js
+++ js/index.js
@@ -1,36 +1,68 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+
+function updateSubtotal(product) {
+  
+  // console.log('Calculating subtotal, yey!');
+  
+  const $price = product.querySelector('.price span');
+  const price = Number($price.innerText);
+  
+  const $quantity = product.querySelector('.quantity input');
+  const quantity = $quantity.valueAsNumber;
+  
+  const subtotal = price * quantity;
+  const $subtotal = product.querySelector('.subtotal span');
+  $subtotal.innerText = subtotal
+  
+  return subtotal;  
+  
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  
+  const allProducts = document.querySelectorAll('.product');
+  
+  let total = 0;
+  
+  for (let i=0; i < allProducts.length; i++) {
+    total = total + updateSubtotal(allProducts[i]);   
+  }
+  
+  const $total = document.querySelector('#total-value span');
+  $total.innerText = total;
 }
 
 // ITERATION 4
 
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+window.addEventListener('click', () => {
+  const itemDeleteButton = document.querySelectorAll('.btn .btn-remove');
+  
+  itemDeleteButton.addEventListener('click', () => {
+    const $itemDeleteButton = target; 
+    const $cellProduct = $itemDeleteButton.parentNode;
+    const $productLine = $cellProduct.parentNode;
+
+    $productLine.removeChild($cellProduct);
+  });
+
   //... your code goes here
-}
+});
 
-// ITERATION 5
 
-function createProduct() {
+function removeProduct(event) {
+    const target = event.currentTarget;
+    console.log('The target in remove is:', target);
+    //... your code goes here
+
+  }
+    
+    
+    
+    // ITERATION 5
+    
+    function createProduct() {
   //... your code goes here
 }
 
